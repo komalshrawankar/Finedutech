7a435d2368f0cc0ae27950f231289f53e398425b
komal shrawankar <shrawankarkomal9@gmail.com>
2026-06-25 18:16:55 +0530

    mobile view3

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,22 +18,22 @@ const HeroSection = () => {
 
           {/* Text Content */}
           {/* Text Content */}
-<div className="absolute left-[13%] top-[18%] z-20 max-w-[430px] sm:left-[12%] sm:top-[17%] md:max-w-[520px] lg:left-[10%] lg:top-[20%] lg:max-w-[640px]">
+<div className="absolute left-[1%] top-[18%] z-20 max-w-[430px] sm:left-[12%] sm:top-[17%] md:max-w-[520px] lg:left-[10%] lg:top-[20%] lg:max-w-[640px]">
   <h1 className="hero-title-font">
-    <span className="block hero-left-animate hero-delay-1 text-black text-[32px] sm:text-[42px] md:text-[52px] lg:text-[64px]">
+    <span className="block hero-left-animate hero-delay-1 text-black text-[22px] sm:text-[42px] md:text-[52px] lg:text-[64px]">
       Financial Skills
     </span>
 
-    <span className="block hero-left-animate hero-delay-2 text-black text-[32px] sm:text-[42px] md:text-[52px] lg:text-[64px]">
+    <span className="block hero-left-animate hero-delay-2 text-black text-[22px] sm:text-[42px] md:text-[52px] lg:text-[64px]">
       Stronger Kids
     </span>
 
-    <span className="block hero-right-animate hero-delay-3 text-[#007BFF] text-[32px] sm:text-[42px] md:text-[52px] lg:text-[64px]">
+    <span className="block hero-right-animate hero-delay-3 text-[#007BFF] text-[22px] sm:text-[42px] md:text-[52px] lg:text-[64px]">
       Brighter Futures
     </span>
   </h1>
 
-  <p className="mt-3 max-w-[380px] text-[10px] leading-[1.55] text-black sm:mt-4 sm:max-w-[430px] sm:text-[12px] md:text-[16px] lg:max-w-[480px]">
+  <p className="mt-3 max-w-[180px] text-[10px] leading-[1.55] text-black sm:mt-4 sm:max-w-[430px] sm:text-[12px] md:text-[16px] lg:max-w-[480px]">
     Finedutech empowers kids with literacy and life skills to build
     confidence, make smart choices, and create a better tomorrow.
   </p>
