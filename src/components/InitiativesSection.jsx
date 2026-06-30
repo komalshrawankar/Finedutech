a7aeb790ca6239570c41f5f7286e08d3838a07a1
Samadkhan4 <samadkhan040204@gmail.com>
2026-06-30 15:03:33 +0530

    updated

--- src/components/InitiativesSection.jsx
+++ src/components/InitiativesSection.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-import img1 from "../assets/images/init1.png";
-import img2 from "../assets/images/init2.png";
-import img3 from "../assets/images/init3.png";
-import img4 from "../assets/images/init4.png";
+import img1 from "../assets/images/1.png";
+import img2 from "../assets/images/2.png";
+import img3 from "../assets/images/3.png";
+import img4 from "../assets/images/4.png";
 
 import hoverImg1 from "../assets/images/card-hover1.png";
 import hoverImg2 from "../assets/images/card-hover2.png";
